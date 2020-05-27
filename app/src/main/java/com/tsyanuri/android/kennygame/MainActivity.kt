74d0ac3f01ba5d081a578017b85b612d8a7eca84
tsyanuri <tsyanuri@gmail.com>
2020-05-27 21:36:47 +0700

    Increasing Score and Making a Timer

--- app/src/main/java/com/tsyanuri/android/kennygame/MainActivity.kt
+++ app/src/main/java/com/tsyanuri/android/kennygame/MainActivity.kt
@@ -2,11 +2,33 @@ package com.tsyanuri.android.kennygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var Score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Score = 0
+
+        object : CountDownTimer(10000,1000){
+            override fun onFinish() {
+                textTimer.text = "Timer : Time's Up "
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+               textTimer.text = "Timer : " + millisUntilFinished/1000
+            }
+
+        }.start()
+    }
+
+    fun increaseScore(view: View){
+        Score++
+
+        textScore.text = "Score : " + Score
     }
 }
