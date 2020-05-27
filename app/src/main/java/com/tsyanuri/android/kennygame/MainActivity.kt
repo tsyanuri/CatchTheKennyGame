6a6016b7631857b997bd52f011df3cd2e2c4331d
tsyanuri <tsyanuri@gmail.com>
2020-05-27 23:47:04 +0700

    Kenny Animates! and Stopping Kenny

--- app/src/main/java/com/tsyanuri/android/kennygame/MainActivity.kt
+++ app/src/main/java/com/tsyanuri/android/kennygame/MainActivity.kt
@@ -3,20 +3,35 @@ package com.tsyanuri.android.kennygame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     var Score = 0
+    var imageArray = ArrayList<ImageView>()
+    var handler : Handler = Handler()
+    var runnable : Runnable = Runnable {  }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Score = 0
 
+        imageArray = arrayListOf(kenny1,kenny2,kenny3,kenny4,kenny5,kenny6,kenny7,kenny8,kenny9)
+
+        hideImage()
+
         object : CountDownTimer(10000,1000){
             override fun onFinish() {
-                textTimer.text = "Timer : Time's Up "
+                textTimer.text = "Time's Up "
+                handler.removeCallbacks(runnable)
+                for (image in imageArray){
+                    image.visibility = View.INVISIBLE
+                }
             }
 
             override fun onTick(millisUntilFinished: Long) {
@@ -24,6 +39,22 @@ class MainActivity : AppCompatActivity() {
             }
 
         }.start()
+    }
+
+    fun hideImage(){
+
+        runnable = object : Runnable{
+            override fun run() {
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+                val randomIndex = Random.nextInt(8-0)
+                imageArray[randomIndex].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable,500)
+            }
+        }
+        handler.post(runnable)
     }
 
     fun increaseScore(view: View){
